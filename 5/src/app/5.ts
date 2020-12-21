5a12511ac26de7ab9d5aa24b816b7ebcc680a9ef
carl <carl@otsu.se>
2020-12-21 13:42:16 +0100

    day 5 part 2

--- 5/src/app/5.ts
+++ 5/src/app/5.ts
@@ -12,12 +12,28 @@ function binaryStep(step:number, input:string, tuple:number[]) {
     }
 }
 
+// Your seat wasn't at the very front or back, though; the seats with IDs +1 and -1 from yours will be in your list.
+function findSeat(seatIds:Set<number>, smallestSeatId:number, biggestSeatId:number) {
+    let possibleSeats = new Set<number>();
+    for (let i = smallestSeatId + 1; i < biggestSeatId - 1; i++) possibleSeats.add(i);
+    seatIds.forEach(seat => {
+        possibleSeats.delete(seat);
+    })
+
+    // sanity check according to rule
+    possibleSeats.forEach(seat => {
+        if (seatIds.has(seat - 1) && seatIds.has(seat +1)) console.log('Possible seat: ', seat)
+    })
+}
+
 
 const run = async(input:string) => {
     let m = promisify(fs.readFile)(input, {encoding: 'utf-8'});
     m.then(file => {
         let lines:string[] = file.split('\n');
-        let biggestSeatId = 0;
+        let biggestSeatId = Number.MIN_SAFE_INTEGER;
+        let smallestSeatId = Number.MAX_SAFE_INTEGER;
+        let seatIds = new Set<number>();
         lines.forEach(boardingPass => {
             let row = [0, 128];
             let step = 128;
@@ -35,9 +51,13 @@ const run = async(input:string) => {
 
             let seatId = row[0] * 8 + column[0];
             if (seatId > biggestSeatId) biggestSeatId = seatId;
-            console.log(boardingPass, 'Row:', row[0], 'Column:', column[0], 'SeatID:', seatId)
+            if (seatId < smallestSeatId) smallestSeatId = seatId;
+            // console.log(boardingPass, 'Row:', row[0], 'Column:', column[0], 'SeatID:', seatId)
+
+            seatIds.add(seatId);
         })
-        console.log('Biggest SeatID: ', biggestSeatId)
+        console.log('Biggest SeatID: ', biggestSeatId, 'Smallest SeatID:', smallestSeatId);
+        findSeat(seatIds, smallestSeatId, biggestSeatId);
     });
 }
 
